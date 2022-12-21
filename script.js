3cb52c3725716825b569a940fb999d97bf76ddf5
DannyAe <52017203+DannyAe@users.noreply.github.com>
2022-12-21 12:13:58 -0500

    Update script.js
    moved js from sandbox to repo

--- script.js
+++ script.js
@@ -1,45 +1,86 @@
-// Assignment Code
+// CREATE VARIABLES AND CHARACTER SETS - THIS IS WHAT WE WILL USE TO POPULATE PASSWORD
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "!@#$%^&*(){}[]<>?|/|";
+var userSelection = "";
+var finalPassword = "";
 
+
+// CREATE A VARIABLE FOR THE #GENERATE FUNCTION
 var generateBtn = document.querySelector("#generate");
 
-var lowerStr = "abcdefghijklmnopqrstuvwxyz"
-var lowerArr = lowerStr.split("")
-// to see this is working console.log (lowerArr)
 
-var upperArr = lowerStr.toUpperCase().split("")
-// console.log (upperArr)
+function generatePassword() {
+  
+  // CLEAR userSelection VAR FOR EACH NEW PASSWORD
+   
+  var userSelection = "";
+  
 
-var numArr  = [0,1,2,3,4,5,6,7,8,9]
+    // CREATE PASSWORD LENGTH
+    passwordLength = prompt("Please enter a password length between 8 and 128 characters.");
+      if (!passwordLength) {
+        alert("You must provide a value!")
+        return generatePassword();
+      } else if (passwordLength < 8)  {
+        alert("Password must be at least 8 characters in length")
+        return generatePassword();
+      } else if (passwordLength > 128) {
+        alert("Password cannot exceed 128 characters in length")
+        return generatePassword();
+      }
 
-var specialArr = "!@#$%^&*".split ("")
-console.log (specialArr)
+    // CHOOSE AND GENERATE CHARACTER TYPE
+    lowerCharConfirm = confirm("Would you like to include lowercase letters in your password?");
+      if (lowerCharConfirm) {
+        userSelection += lowerChar
+      };
+      console.log(lowerCharConfirm);
+      console.log(userSelection);
 
+    upperCharConfirm = confirm("Would you like to include uppercase letters in your password?");
+      if (upperCharConfirm) {
+        userSelection += upperChar
+      };
+      console.log(upperCharConfirm);
+      console.log(userSelection);
 
+    numberCharConfirm = confirm("Would you like to include numbers in your password?");
+      if (numberCharConfirm) {
+        userSelection += numberChar
+      };
+      console.log(numberCharConfirm);
+      console.log(userSelection);
 
-function generatePassword () { 
-// ask user for password length
+      specialCharConfirm = confirm("Would you like to include special characters in your password?");
+        if (specialCharConfirm) {
+          userSelection += specialChar
+        };
+        console.log(specialCharConfirm);
+        console.log(userSelection);
 
-var passwordLen = window.prompt("How long?")
-var totalArr = []
-if(confirm(yes)){
-totalArr = totalArr.concat(lowerArr)
+      // PROMPT IF NO CHARACTER SET WAS SELECTED
+      if (lowerCharConfirm === false &&  upperCharConfirm === false &&  numberCharConfirm === false &&  specialCharConfirm === false) {
+          alert("You must select at least one character type!")
+          generatePassword()
+      };
+      
+    // GENERATE RANDOM PASSWORD BASED ON USERS PREFERENCES
+    finalPassword = ""
+    for (var i = 0; i < passwordLength; i++) {
+      finalPassword += userSelection.charAt(Math.floor(Math.random() * userSelection.length))
+    }
+      
+    return finalPassword
+  }
 
-
-}
-// ask user what type of characters 
-// use for loop to create N number of random characters
-
-return "password"
-}
-
-
-// Write password to the #password input
+// SEND PASSWORD TO #PASSWORD INPUT BOX
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
