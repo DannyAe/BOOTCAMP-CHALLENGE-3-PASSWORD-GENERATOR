aa0136d27d2985097b3a7680c71062dcfe1b67ba
DannyAe <52017203+DannyAe@users.noreply.github.com>
2022-12-23 21:48:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,42 +18,54 @@ function generatePassword() {
   var userSelection = "";
   
 
+
+
+// ------------------ //
+
+
+
     // CREATE PASSWORD LENGTH
-    passwordLength = prompt("Please enter a password length between 8 and 128 characters.");
+    passwordLength = prompt("Please choose length of your password between 8 and 128 characters.");
       if (!passwordLength) {
-        alert("You must provide a value!")
-        return generatePassword();
+        alert("Please add a value!")
+          return generatePassword();
       } else if (passwordLength < 8)  {
-        alert("Password must be at least 8 characters in length")
-        return generatePassword();
+        alert("Password must be at least 8 characters")
+          return generatePassword();
       } else if (passwordLength > 128) {
-        alert("Password cannot exceed 128 characters in length")
-        return generatePassword();
+        alert("Password may not be more than 128 characters")
+         return generatePassword();
       }
 
+
+
+// ------------------ //
+
+
+
     // CHOOSE AND GENERATE CHARACTER TYPE
-    lowerCharConfirm = confirm("Would you like to include lowercase letters in your password?");
+    lowerCharConfirm = confirm("Inclue lowercase characters?");
       if (lowerCharConfirm) {
         userSelection += lowerChar
       };
-      console.log(lowerCharConfirm);
-      console.log(userSelection);
+        console.log(lowerCharConfirm);
+        console.log(userSelection);
 
-    upperCharConfirm = confirm("Would you like to include uppercase letters in your password?");
+    upperCharConfirm = confirm("Include uppercase characters?");
       if (upperCharConfirm) {
         userSelection += upperChar
       };
-      console.log(upperCharConfirm);
-      console.log(userSelection);
+        console.log(upperCharConfirm);
+        console.log(userSelection);
 
-    numberCharConfirm = confirm("Would you like to include numbers in your password?");
+    numberCharConfirm = confirm("Include numbers?");
       if (numberCharConfirm) {
         userSelection += numberChar
       };
-      console.log(numberCharConfirm);
-      console.log(userSelection);
+        console.log(numberCharConfirm);
+        console.log(userSelection);
 
-      specialCharConfirm = confirm("Would you like to include special characters in your password?");
+      specialCharConfirm = confirm("Include special characters?");
         if (specialCharConfirm) {
           userSelection += specialChar
         };
@@ -62,9 +74,16 @@ function generatePassword() {
 
       // PROMPT IF NO CHARACTER SET WAS SELECTED
       if (lowerCharConfirm === false &&  upperCharConfirm === false &&  numberCharConfirm === false &&  specialCharConfirm === false) {
-          alert("You must select at least one character type!")
+          alert("Please select at least one character type!")
           generatePassword()
       };
+
+
+
+// ------------------ //
+
+
+
       
     // GENERATE RANDOM PASSWORD BASED ON USERS PREFERENCES
     finalPassword = ""
@@ -85,3 +104,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// HOPE THIS WORKS!//
